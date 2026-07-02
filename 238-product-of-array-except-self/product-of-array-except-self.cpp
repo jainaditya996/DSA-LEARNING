e2841ea8db1f1302ff4ee0294c11a3fe4123b91b
jainaditya996 <jainaditya0996@gmail.com>
2026-07-02 13:48:29 +0530

    product-of-array-except-self.cpp

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-     int  count =0;
-        for(int x : nums){
-            if(x==0){
-                count++;
-            }
+        int n=nums.size();
+        vector<int>ans(n,1);
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]*nums[i-1];
+           
         }
-     int   n=nums.size();
-        if(count>=2){
-          for(int i=0;i<n;i++){
-            nums[i]=0;
-          }
-        return nums;
+        int suffix=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+           
+            ans[i]*=suffix;
+             suffix*=nums[i];
         }
-        if(count==1){
-            int prod=1;
-            for(int i=0;i<n;i++){
-                if(nums[i]!=0){
-                    prod*=nums[i];
-                }
-            }
-            for(int i=0;i<n;i++){
-                if(nums[i]==0){
-                    nums[i]=prod;
-                    
-                }else{
-                    nums[i]=0;
-                }
-            }
-        }
-        if(count==0){
-            int prod=1;
-              for(int i=0;i<n;i++){
-                if(nums[i]!=0){
-                    prod*=nums[i];
-                }
-            }
-            for(int i=0;i<n;i++){
-                nums[i]=prod/nums[i];
-            }
-        }
-        return nums;
+        return ans;
     }
 };
+
+//code following all the statements 
+//method commonly known as suffix and prefix method
+//in this method i created 1 extra space ans which stores the value of prefixes followed by multiplication of respective suffix in another loop
+
+//here the space complaxity is O(1)
+//while timecomplexity is O(2n)----> which collectively means O(n)
