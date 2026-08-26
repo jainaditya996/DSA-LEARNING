4ae9d5d9fda0e8a8ce64f19c6c879e49a21374d1
jainaditya996 <jainaditya0996@gmail.com>
2026-08-26 14:21:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (99.24%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size()){
+                if(nums[i]!=nums[i+1]){
+                    return false;
+                }
+               i+=2;
+        }
+        return true;
+        
+    }
+};
